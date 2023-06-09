f2d89bb9a8144a1659cdc511e4d926e520b50675
TARUN PRAKASH PORAM <91120274+tarunprakashporam@users.noreply.github.com>
2023-06-09 10:44:34 +0530

    login and sign up

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+const USERS = [];
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.post('/signup', function(req, res) {
+    const { email, password } = req.body;
+    const userExists = USERS.some(user => user.email === email);
+
+    if (userExists) {
+        res.status(400).json({ error: 'User with email already exists' });
+    } else {
+        USERS.push({ email, password });
+    }
+
+    res.status(200).json({ message: 'User signed up successfully' });
+});
+
+app.post('/login', function(req, res) {
+    const { email, password } = req.body;
+    const user = USERS.some(user => user.email === email);
+
+    if (user) {
+        if (user.password == password) {
+            const token = generateRandomToken();
+            res.status(200).json({ token: token, message: "Login successful" });
+        } else {
+            res.status(401).json({ message: "Password incorrect" });
+        }
+    } else {
+        res.status(401).json({ error: 'User does not exist' });
+    }
+});
+
+app.listen(port, function() {
+    console.log(`Example app listening on port ${port}`);
+});
